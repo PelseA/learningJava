3ca88dc1672383a2e4304917f741fc933eea6a45
Aleksandra Pelse <shashka_aya@mail.ru>
2020-04-15 21:58:19 +0300

    убрала лишнее поле

--- src/ru/pelse/syntax/seabattle/Abc.java
+++ src/ru/pelse/syntax/seabattle/Abc.java
@@ -1,30 +1,24 @@
 package ru.pelse.syntax.seabattle;
 
 public enum Abc {
-    A(0, 'a'), B(1, 'b'), C(2, 'c'),
-    D(3, 'd'), E(4, 'e'), F(5, 'f'),
-    G(6, 'g'), H(7, 'h'), I(8, 'i'),
-    J(9, 'j'), K(10, 'k'), L(11, 'l'),
-    M(12, 'm'), N(13, 'n'), O(14, 'o'),
-    P(15, 'p'), Q(16, 'q'), R(17, 'r'),
-    S(18, 's'), T(19, 't'), U(20, 'u'),
-    V(21, 'v'), W(22, 'w'), X(23, 'x'),
-    Y(24, 'y'), Z(25, 'z');
+    A(0), B(1), C(2),
+    D(3), E(4), F(5),
+    G(6), H(7), I(8),
+    J(9), K(10), L(11),
+    M(12), N(13), O(14),
+    P(15), Q(16), R(17),
+    S(18), T(19), U(20),
+    V(21), W(22), X(23),
+    Y(24), Z(25);
 
     private final int number;
-    private final char letter;
 
-    Abc(int number, char letter) {
+    Abc(int number) {
         this.number = number;
-        this.letter = letter;
     }
 
     public int getNumber() {
         return number;
-    }
-
-    public char getLetter() {
-        return letter;
     }
 
 }
