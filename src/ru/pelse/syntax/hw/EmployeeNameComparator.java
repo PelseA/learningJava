f339b0415d1171115edf46199b611eb63176ff91
Aleksandra Pelse <shashka_aya@mail.ru>
2020-04-21 00:53:34 +0300

    компараторы для Employee

---
+++ src/ru/pelse/syntax/hw/EmployeeNameComparator.java
@@ -0,0 +1,35 @@
+package ru.pelse.syntax.hw;
+
+import java.util.Comparator;
+
+//один компаратор на имя, второй на зп, третий на возраст, четвёртый на компанию
+
+class EmployeeNameComparator implements Comparator<Employee> {
+    @Override
+    public int compare(Employee o1, Employee o2) {
+        return o1.getName().compareTo(o2.getName());
+    }
+}
+
+class EmployeeSalaryComparator implements Comparator<Employee> {
+    @Override
+    public int compare(Employee o1, Employee o2) {
+        return Integer.compare(o1.getSalary(), o2.getSalary());
+    }
+}
+
+class EmployeeAgeComparator implements Comparator<Employee> {
+    @Override
+    public int compare(Employee o1, Employee o2) {
+        return Integer.compare(o1.getAge(), o2.getAge());
+    }
+}
+
+class EmployeeCompanyComparator implements Comparator<Employee> {
+    @Override
+    public int compare(Employee o1, Employee o2) {
+        return o1.getCompany().compareTo(o2.getCompany());
+    }
+}
+
+
