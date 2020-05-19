2ea9ee37d537ebb9aac8aab2935b9f2df63e8d1f
Aleksandra Pelse <shashka_aya@mail.ru>
2020-05-20 00:38:38 +0300

    Добавила limit10 и сортировку с помощью Stream в getTopRepeatingWords (класс TextHandler); изменила тестовый список слов; заменила регулярку для пунктуации; улучшила вывод 'sout' в frequencyLetters (класс TextHandler)

--- src/ru/pelse/syntax/maps/MapTask.java
+++ src/ru/pelse/syntax/maps/MapTask.java
@@ -3,10 +3,7 @@ package ru.pelse.syntax.maps;
 import java.util.*;
 
 public class MapTask {
-    // 1. написать статический метод, который приннимает на вход
-    // мапу (firstTaskMap) и город (city)
-    // и формирует список (List) логинов, которые соответствуют переданному городу
-    private static List<String> getPeopleByCity(HashMap<String, String> map, String city) {
+    private static List<String> getPeopleByCity(Map<String, String> map, String city) {
         List<String> logins = new ArrayList<>();
         for (Map.Entry<String, String> entry : map.entrySet()) {
             if (entry.getValue().equals(city)) {
@@ -16,26 +13,22 @@ public class MapTask {
         return logins;
     }
 
-    // 2. дан список слов (words). Написать метод, который будет возвращать количество одинаковых слов в списке
-    // в виде Map<String, Integer>, где String - слово и Integer - количество повторений
     private static Map<String, Integer> getRepeatingWords(List<String> words) {
-        Map<String, Integer> repeatingWordsMap = new HashMap<>();
+        Map<String, Integer> wordCounter = new HashMap<>();
         for (String word : words) {
-            int count = 0;
-            for (String w : words) {
-                if (word.equals(w)) {
-                    count++;
-                }
+            if (wordCounter.containsKey(word)) {
+                wordCounter.replace(word, wordCounter.get(word) + 1);
+            } else {
+                wordCounter.put(word, 1);
             }
-            repeatingWordsMap.put(word, count);
         }
-        return repeatingWordsMap;
+        return wordCounter;
     }
 
     // 3. дана мапа (customerMap).
     // Написать метод, который принимает на вход аргументы int from и int to и возвращает новую мапу,
     // в которую войдут все покупатели, возраст которых находится в диапазоне [from, to)
-    private static Map<String, Customer> getCustomerInRange(HashMap<String, Customer> customers, int from, int to ) {
+    private static Map<String, Customer> getCustomerInRange(HashMap<String, Customer> customers, int from, int to) {
         HashMap<String, Customer> customerMap = new HashMap<>();
         int order = 1;
         for (Map.Entry<String, Customer> entry : customers.entrySet()) {
@@ -48,31 +41,41 @@ public class MapTask {
     }
 
     public static void main(String[] args) {
-        // 1.
+        // 1. написать статический метод, который приннимает на вход
+        // мапу (firstTaskMap) и город city
+        // и формирует список (List) логинов, которые соответствуют переданному городу
         HashMap<String, String> firstTaskMap = new HashMap<>();
+
         firstTaskMap.put("qwe", "Тверь");
         firstTaskMap.put("asd", "Тверь");
         firstTaskMap.put("zxc", "Москва");
         firstTaskMap.put("rty", "Тверь");
         firstTaskMap.put("fgh", "Магадан");
-
         String city = "Тверь";
 
         System.out.println(getPeopleByCity(firstTaskMap, city));
 
-        // 2.
+        // 2. дан список слов (words). Написать метод, который будет возвращать количество одинаковых слов в списке
+        // в виде Map<String, Integer>, где String - слово и Integer - количество повторений
+        String s = "May, dinosaur, whale, may, frog, tactful, brave, people, indistinguishable, supernatural, dog, frog, " +
+                "August, tremendous, desired, frog, Monday, apple, July, whale, supernatural, polish, save, main, aboard, " +
+                "dinosaur, tremendous, reflect, west, frog, brave, artsy, whale, desired, benevolent, major, whale, doubt, " +
+                "brave, among, August, people, July, frog, July, may, development, developer, system, supernatural, math, " +
+                "benevolent, obsessive, major, whale, doubt, frog, brave, among, August, people, July, tactful, desired, " +
+                "tactful, system, tremendous, frog, benevolent, System, energy, begging, artsy, vine, south, brave, july, " +
+                "embarrassed, supernatural, math, geometry, polish, frog, independent, tactful, supernatural, brave, people, " +
+                "independent, tactful, frog, indistinguishable, artsy, desired, Monday, tremendous, among, whale, developer, " +
+                "apple, whale, apple, dinosaur, tremendous, affective, benevolent, dinosaur, desired, system, elementary, " +
+                "tremendous, benevolent, tremendous, System, development, frog, energy, surgery, surgery, benevolent, desired, " +
+                "benevolent, development, frog, doubt, whale, doubt, brave, supernatural, among, August, people, math, energy, " +
+                "obsessive, major, whale, doubt, brave, supernatural, among, August, people, July, tactful, space, may, " +
+                "interrupt, unmindful, ordinary, game, supernatural, tremendous, benevolent, tremendous, System, energy, " +
+                "surgery, frog, major, whale, doubt, frog, brave, supernatural, development, dog, tactful, frog, dog";
         List<String> words = new ArrayList<>();
-        words.add("may");
-        words.add("august");
-        words.add("june");
-        words.add("may");
-        words.add("may");
-        words.add("july");
-        words.add("may");
-        words.add("august");
-        words.add("august");
+        String[] arr = s.split(", ");
+        Collections.addAll(words, arr);
 
-        System.out.println(getRepeatingWords(words));
+        System.out.println("Повторяющиеся слова " + getRepeatingWords(words));
 
         // 3.
         HashMap<String, Customer> customerMap = new HashMap<>();
@@ -90,22 +93,23 @@ public class MapTask {
         // 4.3. написать метод, который выводит в консоль топ 10 самых частых слов
         // 4.4. Вывести частоту встречаемости букв анг алфавита в данном тексте. Вывести в процентах для каждой буквы
 
-        String text = "It is a uncover long established fact that a reader will be distracted uncover by the readable content of a page " +
+        String text = "Русские буквы It is a uncover long established fact that a reader will be distracted uncover by the readable content of a page " +
                 "when looking at its layout The point of using uncover Lorem Ipsum is that sites it has a more-or-less normal distribution of letters" +
                 "as uncover opposed to still using Content here humour uncover content here making it look like readable English Many desktop publishing " +
                 "packages and web page editors now use Lorem Ipsum as their default model text and a search for lorem ipsum will " +
                 "uncover many web sites still uncover in their infancy Various versions uncover have evolved over the years uncover sometimes by accident" +
                 " sometimes on purpose injected humour and the like"; // !!! в тексте содержатся только буквы и пробельные символы !!!
 
-
         // 4.1.
-        System.out.println(TextHandler.repeatingWord("It", text));
+        String testString = "Lorem";
+        System.out.println("Слово " + testString + " встречается в тексте " + TextHandler.repeatingWord(testString, text) + " раз(а)");
         // 4.2.
         System.out.println(TextHandler.groupByWordLength(text));
         // 4.3.
-
+        System.out.println("Топ 10 самых частых слов в списке: ");
+        System.out.println(TextHandler.getTopRepeatingWords(words));
         // 4.4.
-        System.out.println(TextHandler.frequencyLetters(text));
+        TextHandler.frequencyLetters(text);
 
     }
 
--- src/ru/pelse/syntax/maps/TextHandler.java
+++ src/ru/pelse/syntax/maps/TextHandler.java
@@ -1,7 +1,7 @@
 package ru.pelse.syntax.maps;
 
-import java.text.DecimalFormat;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class TextHandler {
     // 4.1. написать метод, принимающий на вход слово и возвращающий частоту встречаемости данного слова в тексте
@@ -13,6 +13,8 @@ public class TextHandler {
         }
         ArrayList<String> textToList = new ArrayList<>(Arrays.asList(lowerText));
         return Collections.frequency(textToList, word.toLowerCase());
+
+        // return Collections.frequency(Arrays.asList(text.toLowerCase().split(" ")),word.toLowerCase());
     }
 
     // 4.2. написать метод, который собирает все слова в группы по количеству букв:
@@ -22,13 +24,15 @@ public class TextHandler {
         HashSet<String> words;
         String[] textSplit = text.split(" ");
         for (String word : textSplit) {
-            words = new HashSet<>();
             if (!group.containsKey(word.length())) {
+                words = new HashSet<>();
                 words.add(word.toLowerCase());
                 group.put(word.length(), words);
             } else {
                 group.get(word.length()).add(word.toLowerCase());
             }
+            // 1=[a], 2=[as, be, at, in, by, of, is, it, to, on], 3=[the, web, and, use, now, for, its, has], 4=[here, over, fact,
+            // 1=[a], 2=[as, be, at, in, by, of, is, it, to, on], 3=[the, web, and, use, now, for, its, has], 4=[here, over, fact, will, like
         }
         return group;
     }
@@ -37,43 +41,44 @@ public class TextHandler {
     // 2. дан список слов (words). Написать метод, который будет возвращать количество одинаковых слов в списке
     // в виде Map<String, Integer>, где String - слово и Integer - количество повторений
 
-    //todo 4.3. не дописан
-    static public Map<String, Integer> getRepeatingWords(List<String> words) {
-        Map<String, Integer> repeatingWordsMap = new HashMap<>();
+    static public Map<String, Integer> getTopRepeatingWords(List<String> words) {
+        Map<String, Integer> map = new HashMap<>();
         for (String word : words) {
             int count = 1;
             for (String w : words) {
-                if (word.equals(w)) {
+                if (word.equalsIgnoreCase(w)) {
                     count++;
                 }
             }
-            repeatingWordsMap.put(word, count-1);
+            map.put(word.toLowerCase(), count - 1);
         }
-        return repeatingWordsMap;
+
+        return map.entrySet().stream()
+                .sorted(Map.Entry.<String, Integer>comparingByValue().reversed())
+                .limit(10)
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
     }
 
-
-
     // 4.4. Вывести частоту встречаемости букв анг алфавита в данном тексте. Вывести в процентах для каждой буквы
-    static public HashMap<Character, String> frequencyLetters(String text) {
-        String textWithoutSpaces = text.replaceAll(" ", "");
-        char[] letters = textWithoutSpaces.toCharArray();
-        int countLetters = letters.length;
-        for (int k = 0; k < countLetters; k++) {
-            letters[k] = Character.toLowerCase(letters[k]);
+    static public void frequencyLetters(String processedText) {
+        String text = processedText.toLowerCase()
+                .replaceAll("\\p{P}", ""); //убрали пунктуацию
+
+        int countCharacters = text.length();
+        text = text.replaceAll("[^a-z]", ""); //удалил все нелатинские
+        Map<Character, Integer> frequentCharacters = new TreeMap<>();
+        for (Character character : text.toCharArray()) {
+            if (frequentCharacters.containsKey(character))
+                frequentCharacters.replace(character, frequentCharacters.get(character) + 1);
+            else frequentCharacters.put(character, 1);
         }
-        HashMap<Character, String> freqLetters = new HashMap<>();
-        int i = 0;
-        while (i < countLetters) {
-            int count = 0;
-            for (char letter : letters) {
-                if (letters[i] == letter) count++;
-            }
-            System.out.println(letters[i]);
-            float percent = (float)count * 100 / countLetters;
-            freqLetters.put(letters[i], percent + "%");
-            i++;
+        System.out.println("Процент латинских букв в тексте: ");
+        System.out.println("всего букв в тексте " + countCharacters);
+        Set<Map.Entry<Character, Integer>> frequentCharactersSet = frequentCharacters.entrySet();
+        for (Map.Entry<Character, Integer> character : frequentCharactersSet) {
+            System.out.print(character.getKey() + " встретилась в тексте " + character.getValue()
+                    + " раз, что составляет " +
+                    + (character.getValue().floatValue() / countCharacters * 100) + " % \n");
         }
-        return  freqLetters;
     }
 }
